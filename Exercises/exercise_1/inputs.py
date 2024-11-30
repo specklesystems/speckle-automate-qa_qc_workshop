d43fb0be99b751a5fb44111dc336d7e457080074
Jonathon Broughton <760691+jsdbroughton@users.noreply.github.com>
2024-11-30 12:59:25 +0000

    Update inputs.py

--- Exercises/exercise_1/inputs.py
+++ Exercises/exercise_1/inputs.py
@@ -4,19 +4,21 @@ from speckle_automate import AutomateBase
 
 class FunctionInputs(AutomateBase):
     """These are function author defined values.
-
+    
     Automate will make sure to supply them matching the types specified here.
     Please use the pydantic model schema to define your inputs:
     https://docs.pydantic.dev/latest/usage/models/
     """
-
+    
+    # Original input from exercise 0
     comment_phrase: str = Field(
-      title="Comment Phrase",
-      description="This phrase will be added to a random model element.",
+        title="Comment Phrase",
+        description="This phrase will be added to a random model element.",
     )
-
-    # We now want to specify the number of elements to which the comment phrase will be added.
+    
+    # NEW in Exercise 1: Added field to specify how many elements to process
+    # This allows users to control the scope of the automation
     number_of_elements: int = Field(
-      title="Number of Elements",
-      description="The number of elements to which the comment phrase will be added.",
+        title="Number of Elements",
+        description="The number of elements to which the comment phrase will be added.",
     )
